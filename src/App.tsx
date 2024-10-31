af3055390ccbe7366d82fb7550ac3a22db945d33
Valentino Valentin <mothaflora@Valentinos-MacBook-Pro.local>
2024-10-31 04:06:25 -0600

    Add event data

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import EventRadar from './componets/EventRadar';
 
+
 function App() {
+ const [events, setEvents] = useState(null);
+
+  useEffect(() => {
+    fetch('/mockEvents.json')
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+        }
+        return response.json();
+        })
+        .then((data) => {
+        setEvents(data);
+      })
+      .catch((error) => {
+        console.error("Error loading data:", error);
+      });
+  }, []);
+
   return (
     <div className="App">
       <EventRadar />
+      <div>
+        {/* refactor conditional rendering below */}
+      {events ? (
+          <pre>{JSON.stringify(events, null, 2)}</pre>
+        ) : (
+          <p>Loading events...</p>
+        )}
+      </div>
     </div>
   );
 }
